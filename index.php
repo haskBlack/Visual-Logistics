b21edf691b86a84f5dec8e776c92b9aab1cc41a5
haskBlack <94982569+haskBlack@users.noreply.github.com>
2024-12-23 14:18:18 -0600

    Initial

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VISUAL LOGISTICS VL</title>
+    <link rel="stylesheet" href="Assets/CSS/style-login.css">
+</head>
+<body>
+    <div class="header">
+        <h1 class="title">VISUAL LOGISTICS</h1>
+    </div>
+    
+</body>
+</html>
